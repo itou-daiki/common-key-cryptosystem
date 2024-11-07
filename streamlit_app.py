292f714e34c57f60145e697cfc2b4d20725ef1d4
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2024-11-07 13:03:40 +0000

    修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,18 @@ st.set_page_config(page_title="共通鍵暗号体験（シーザー暗号体験�
 st.title("共通鍵暗号体験（シーザー暗号体験）")
 st.write("")
 st.subheader("ブラウザでシーザー暗号の「暗号化」→「復号」まで体験することができます")
+st.write("")
+st.subheader("シーザー暗号とは")
+st.write("シーザー暗号は、古代ローマの将軍であるガイウス・ユリウス・カエサル（Julius Caesar）が使ったとされる暗号です。")
+st.write("シーザー暗号は、アルファベットの文字を一定の数だけずらすことで暗号化を行います。")
+st.write("例えば、シフト値が3の場合、A→D、B→E、C→F、...、X→A、Y→B、Z→C というように暗号化されます。")
+st.write("この暗号化方法は、暗号化と復号の両方に同じシフト値を使う「共通鍵暗号」の一種です。")
 
-tab1, tab2 = st.tabs([ "暗号化", "復号"])
+
+tab1, tab2 = st.tabs([ "ステップ１：暗号化", "ステップ２：復号"])
 
 with tab1:
-    st.subheader("暗号化")
+    st.subheader("ステップ１：暗号化")
     
     plaintext = st.text_input("暗号化したいテキストを入力してください（半角アルファベットのみ）：")
     shift_encrypt = st.number_input("シフト値（鍵）を入力してください：", min_value=1, max_value=25, value=3, key="encrypt")
@@ -32,13 +39,14 @@ with tab1:
             if is_valid_input(plaintext):
                 encrypted_text = caesar_encrypt(plaintext, shift_encrypt)
                 st.success("暗号化されたテキスト："+encrypted_text)
+                st.subheader("「ステップ２：複号」タブに進み、暗号文を入力し、復号を行ってください")
             else:
                 st.error("エラー：入力は半角アルファベット（A-Z, a-z）のみを使用してください。")
         else:
             st.error("暗号化するテキストを入力してください。")
 
 with tab2:
-    st.subheader("復号")
+    st.subheader("ステップ２：復号")
 
     ciphertext = st.text_input("復号したい暗号文を入力してください（半角アルファベットのみ）：")
     shift_decrypt = st.number_input("シフト値（鍵）を入力してください：", min_value=1, max_value=25, value=shift_encrypt, key="decrypt")
